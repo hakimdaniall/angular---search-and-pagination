6afd829814347e55518de92feb06505aa50c8e24
Hakim Danial <dev.hakimdanial@gmail.com>
2022-03-25 00:52:03 +0800

    change styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'uni-angular';
+  url = `http://universities.hipolabs.com/search?country=United+States`;
+  list: any = [];
+  searchText: any;
+  pageOfItems!: Array<any>;
+
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  ngOnInit(): void {
+    this.http.get(this.url).subscribe(Response => {      
+      this.list = Response
+      console.log(this.list)
+    });
+  }
+
+  onChangePage(pageOfItems: Array<any>) {
+    // update current page of items
+    this.pageOfItems = pageOfItems;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HttpClientModule } from '@angular/common/http';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+import { JwPaginationModule } from 'jw-angular-pagination';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    JwPaginationModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    Ng2SearchPipeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
